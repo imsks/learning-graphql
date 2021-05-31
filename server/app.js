a63990ccda179c28f9d8bf232b89a2b5cb1110a7
Sachin <sachinkshuklaoo7@gmail.com>
2021-05-31 08:30:54 +0530

    express with graphql ready

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({}));
 
 app.listen(8000, () => console.log("Server started"));
