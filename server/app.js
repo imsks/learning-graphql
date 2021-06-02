0faa3998f0ea5f11e1c972ed84401d241de87600
Sachin <sachinkshuklaoo7@gmail.com>
2021-06-02 07:11:33 +0530

    base graphql schema created

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(8000, () => console.log("Server started"));
