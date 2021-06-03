3bac5cca6a2c7e8d837c7305a803ee53e1db19bf
Sachin <sachinkshuklaoo7@gmail.com>
2021-06-03 09:05:14 +0530

    mongoose created

--- server/app.js
+++ server/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config("/.env");
 
 const app = express();
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("Connected to Database");
+});
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true,
   })
 );
 
